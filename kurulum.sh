573403616d0a20e86d041d234776d9835d056b53
lixen <58229049+Iixen@users.noreply.github.com>
2020-03-27 03:15:37 +0300

    Add files via upload

---
+++ kurulum.sh
@@ -0,0 +1,1 @@
+pkg install update && pkg install upgrade && pkg install python && pkg install python2 && pkg install python3 && pkg install pip && apt install pip && pkg install toilet && pkg install nano && pkg install requirements
