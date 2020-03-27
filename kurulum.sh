d19f7632eb86624a232cb87f836e0c6f0dd4dadb
lixen <58229049+Iixen@users.noreply.github.com>
2020-03-27 03:40:53 +0300

    Update kurulum.sh

--- kurulum.sh
+++ kurulum.sh
@@ -1,1 +1,14 @@
-pkg install update && pkg install upgrade && pkg install python && pkg install python2 && pkg install python3 && pkg install pip && apt install pip && pkg install toilet && pkg install nano && pkg install requirements
+pkg install php -y	
+pkg install perl -y	
+pkg install nano -y	
+pkg install python python2 python3 -y	pkg install pip pip2 -y
+pip install --upgrade pip
+pip install bs4 
+pip install future 	
+pip install requests 	
+pip install colorama	
+pip install wordlist	
+pkg install termux-api -y	
+pkg install curl -y	
+pkg install wget -y	
+pkg install neofetch -y
